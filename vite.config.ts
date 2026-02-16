258d899ab85f4f6aa640d55a1e8e49dbcae8a2fb
Lizzy0919 <oqadapa737@gmail.com>
2026-02-16 14:30:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 关键修复：把这里的名字换成你的 GitHub 仓库名，注意前后都要有斜杠 /
+  // 注意：前后一定要有斜杠 / ！！！
   base: '/lab-2026/', 
   plugins: [react()],
 })
